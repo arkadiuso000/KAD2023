ad8536bd1d17f6ec3db7ce61cde619561080b0e6
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2023-10-24 16:23:40 +0200

    test

--- zadanie1/test.py
+++ zadanie1/test.py
@@ -6,5 +6,6 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
+
 columns = ["dlugosc dzialki kielicha", "szerokosc dzialki kielicha", "dlugosc platka", "szerokosc platka", "gatunek"]
 myFile = pd.read_csv("././Dane-20231023/data.csv", header=None, names=columns)
