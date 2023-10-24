1cfd26cd36707c33dccbab800a1e3e4da280409b
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2023-10-24 22:53:39 +0200

    mediana do poprawy

--- zadanie1/test.py
+++ zadanie1/test.py
@@ -3,9 +3,9 @@ import matlib as ml
 import statistics as stat
 import math
 import pandas as pd
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-
 
 # wczytuje plik csv
 columns = ["sepal lengtha", "sepal width", "petal length", "petal width", "species"]
@@ -23,12 +23,13 @@ myData['species'] = myData['species'].replace(mapowanie_gatunkow)
 # oblicza liczebność poszczególnych gatunków
 liczebnosc_gatunkow = myData['species'].value_counts()
 # oblicza udział procentowy poszczególnych gatunków
-#normalize=True oznacza ze chcemy wynik miec w procentach domyslnie jest false
+# normalize=True oznacza ze chcemy wynik miec w procentach domyslnie jest false
 udzial_procentowy = (myData['species'].value_counts(normalize=True) * 100).round(2)
-print("Liczebność poszczególnych gatunków:")
-print(liczebnosc_gatunkow)
-print("\nUdział procentowy poszczególnych gatunków:")
-print(udzial_procentowy)
+# print("Liczebność poszczególnych gatunków:")
+# print(liczebnosc_gatunkow)
+# print("\nUdział procentowy poszczególnych gatunków:")
+# print(udzial_procentowy)
+
 
 # PUNKT 2.
 
@@ -39,6 +40,7 @@ def wyznaczMaksimum(lista):
             maksimum = wartosc
     return maksimum
 
+
 def wyznaczMinimum(lista):
     maksimum = 9999
     for wartosc in lista:
@@ -46,11 +48,13 @@ def wyznaczMinimum(lista):
             maksimum = wartosc
     return maksimum
 
+
 def wyznaczSredniaArytmetyczna(lista):
     suma = 0
     for wartosc in lista:
         suma += wartosc
     return suma / len(lista)
+
 
 def wyznaczOdchylenieStandardowe(lista):
     sredniaArytmetyczna = wyznaczSredniaArytmetyczna(lista)
@@ -59,7 +63,44 @@ def wyznaczOdchylenieStandardowe(lista):
     for wartosc in lista:
         sumaKwadratowroznic += (wartosc - sredniaArytmetyczna) ** 2
 
-    odchylenieStandardowe = math.sqrt(sumaKwadratowroznic / (len(lista) -1))
+    odchylenieStandardowe = math.sqrt(sumaKwadratowroznic / (len(lista) - 1))
     return odchylenieStandardowe
-a = [1,2,3,4,5,6]
-print(wyznaczOdchylenieStandardowe(a))
+
+def wyznaczMediane(lista):
+    posortowana_lista = sorted(lista)
+
+    srodek = len(posortowana_lista) // 2
+
+    if len(posortowana_lista) % 2 == 0:
+        print((posortowana_lista[srodek-1] + posortowana_lista[srodek]) / 2)
+        return (posortowana_lista[srodek-1] + posortowana_lista[srodek]) / 2
+    else:
+        return posortowana_lista[srodek]
+
+def wyznaczTrzyKwartyle(lista):
+    listaKwartyli = []
+    srodek = (len(lista)) // 2
+    posortowanaLista = sorted(lista)
+    print("srodek glownej tabeli {}".format(srodek) )
+    if (len(posortowanaLista) % 2 == 0):
+        listaKwartyli.append(wyznaczMediane(posortowanaLista[:srodek]))
+    else:
+        listaKwartyli.append(wyznaczMediane(posortowanaLista[:srodek+1]))
+    listaKwartyli.append(wyznaczMediane(posortowanaLista))
+    listaKwartyli.append(wyznaczMediane(posortowanaLista[srodek:]))
+    return listaKwartyli
+    #do nothing
+    print()
+
+
+
+
+a = [1, 2, 3, 4, 5, 6]
+
+# a = range(0,101)
+
+print("moje kwantyle:       {}".format(wyznaczTrzyKwartyle(a)))
+
+print("poprawne kwantyle:   [{}, {}, {}]".format(np.quantile(a,1/4),np.quantile(a,1/2),np.quantile(a,3/4)))
+
+
