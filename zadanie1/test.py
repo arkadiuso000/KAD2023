89f787c0b3d1ecd2a25be047b2ea7678a31f40af
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2023-10-25 20:31:29 +0200

    pierwszy wykres zrobiony

--- zadanie1/test.py
+++ zadanie1/test.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matlib as ml
+import matplotlib.pyplot as plt
 import statistics as stat
 import math
 import pandas as pd
@@ -39,7 +39,6 @@ print("Razem: {}".format(math.ceil(calosProcent)))
 print("\n\n")
 
 
-
 # PUNKT 2.
 
 def wyznaczMaksimum(lista):
@@ -75,16 +74,18 @@ def wyznaczOdchylenieStandardowe(lista):
     odchylenieStandardowe = math.sqrt(sumaKwadratowroznic / (len(lista) - 1))
     return odchylenieStandardowe
 
+
 def wyznaczMediane(lista):
     posortowana_lista = sorted(lista)
 
-    #odejmuje jeden aby dzialac na indeksach tablicy
+    # odejmuje jeden aby dzialac na indeksach tablicy
     srodek = (len(posortowana_lista) - 1) // 2
 
     if len(posortowana_lista) % 2 == 0:
         return (posortowana_lista[srodek] + posortowana_lista[srodek + 1]) / 2
     else:
-        return  float(posortowana_lista[srodek])
+        return float(posortowana_lista[srodek])
+
 
 def wyznaczTrzyKwartyle(lista):
     # wg prezentacji na wykladzie: W_2_Statystyka_Miary_położenia strona 73
@@ -97,11 +98,11 @@ def wyznaczTrzyKwartyle(lista):
     listaPosortowana = sorted(lista)
     srodek = (len(listaPosortowana) - 1) // 2
 
-    listaKwartyli.append(wyznaczMediane(listaPosortowana[:srodek+1]))
+    listaKwartyli.append(wyznaczMediane(listaPosortowana[:srodek + 1]))
     # print("pierwszy podzbior:           {}".format(listaPosortowana[:srodek+1]))
     listaKwartyli.append(wyznaczMediane(listaPosortowana))
     if len(listaPosortowana) % 2 == 0:
-        listaKwartyli.append(wyznaczMediane(listaPosortowana[srodek+1:]))
+        listaKwartyli.append(wyznaczMediane(listaPosortowana[srodek + 1:]))
         # print("drugi podzbior:              {}".format(listaPosortowana[srodek+1:]))
     else:
         listaKwartyli.append(wyznaczMediane(listaPosortowana[srodek:]))
@@ -109,28 +110,68 @@ def wyznaczTrzyKwartyle(lista):
 
     return listaKwartyli
 
-
-
-
-# a = [1, 2, 3, 4, 5, 6, 7]
-# print("Mediana a: {}".format(wyznaczMediane(a)))
-# # a = range(0,101)
-#
-# print("moje kwartyle:       {}".format(wyznaczTrzyKwartyle(a)))
-#
-# print("numpaja kwartyle:    [{}, {}, {}]".format(np.quantile(a,1/4),np.quantile(a,1/2),np.quantile(a,3/4)))
-#
-
-tytulTabeli = "Cecha    Minimum     Sr +- odch stand    Mediana (Q1-Q3)     Maksimum"
-row1 = "DlDzialki      {}      {}      {}      {}".format(wyznaczMinimum(myData["sepal length"]),wyznaczSredniaArytmetyczna(myData["sepal length"]),  wyznaczTrzyKwartyle(myData["sepal length"]), wyznaczMaksimum(myData["sepal length"]))
-# print(tytulTabeli)
-# print(row1)
-dlugoscDzialki = "\tDługość działki kielicha (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(wyznaczMinimum(myData["sepal length"]),wyznaczSredniaArytmetyczna(myData["sepal length"]),wyznaczOdchylenieStandardowe(myData["sepal length"]),  wyznaczTrzyKwartyle(myData["sepal length"]), wyznaczMaksimum(myData["sepal length"]))
-szerokoscDzialki = "\tSzerokość działki kielicha (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(wyznaczMinimum(myData["sepal width"]),wyznaczSredniaArytmetyczna(myData["sepal width"]),wyznaczOdchylenieStandardowe(myData["sepal width"]),  wyznaczTrzyKwartyle(myData["sepal width"]), wyznaczMaksimum(myData["sepal width"]))
-dlugoscPlatka = "\tDługość płatka (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(wyznaczMinimum(myData["petal length"]),wyznaczSredniaArytmetyczna(myData["petal length"]),wyznaczOdchylenieStandardowe(myData["petal length"]),  wyznaczTrzyKwartyle(myData["petal length"]), wyznaczMaksimum(myData["petal length"]))
-szerokoscPlatka = "\tSzerokość płatka (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(wyznaczMinimum(myData["petal width"]),wyznaczSredniaArytmetyczna(myData["petal width"]),wyznaczOdchylenieStandardowe(myData["petal width"]),  wyznaczTrzyKwartyle(myData["petal width"]), wyznaczMaksimum(myData["petal width"]))
+dlugoscDzialki = "\tDługość działki kielicha (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(
+    wyznaczMinimum(myData["sepal length"]), wyznaczSredniaArytmetyczna(myData["sepal length"]),
+    wyznaczOdchylenieStandardowe(myData["sepal length"]), wyznaczTrzyKwartyle(myData["sepal length"]),
+    wyznaczMaksimum(myData["sepal length"]))
+szerokoscDzialki = "\tSzerokość działki kielicha (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(
+    wyznaczMinimum(myData["sepal width"]), wyznaczSredniaArytmetyczna(myData["sepal width"]),
+    wyznaczOdchylenieStandardowe(myData["sepal width"]), wyznaczTrzyKwartyle(myData["sepal width"]),
+    wyznaczMaksimum(myData["sepal width"]))
+dlugoscPlatka = "\tDługość płatka (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(
+    wyznaczMinimum(myData["petal length"]), wyznaczSredniaArytmetyczna(myData["petal length"]),
+    wyznaczOdchylenieStandardowe(myData["petal length"]), wyznaczTrzyKwartyle(myData["petal length"]),
+    wyznaczMaksimum(myData["petal length"]))
+szerokoscPlatka = "\tSzerokość płatka (cm)\nMinimum:            {}\nSr arytm + odch:    {} +- {}\nMediana(Q1,Q2,Q3):  {}\nMaksimum:           {}\n\n".format(
+    wyznaczMinimum(myData["petal width"]), wyznaczSredniaArytmetyczna(myData["petal width"]),
+    wyznaczOdchylenieStandardowe(myData["petal width"]), wyznaczTrzyKwartyle(myData["petal width"]),
+    wyznaczMaksimum(myData["petal width"]))
 print("\tPUNKT 2\n")
 print(dlugoscDzialki)
 print(szerokoscDzialki)
 print(dlugoscPlatka)
 print(szerokoscPlatka)
+
+# PUNKT 3. - wykresy
+
+
+nazwyWykresow = ["Długość działki kielicha", "Szerokość działki kielicha", "Długość płatka", "Szerokość płatka"]
+        #dlugosc dzialki kielicha
+#rozmiar calego obrazka
+plt.figure(figsize=(16, 8))
+#tworzy dwa wykresy na jednym obrazku
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+                        #rysowanie histogramu
+axs[0].hist(myData[columns[0]], bins=10, alpha=0.5, edgecolor='black', color='green')
+#ustawia os x
+axs[0].set_xlim(4.0,8.0)
+#wlacza siatke
+axs[0].grid(True, linewidth=0.5)
+#opisy z wlasciwosciami
+axs[0].set_ylabel('Liczebność',fontsize=16, fontweight='bold')
+axs[0].set_xlabel('Długość (cm)',fontsize=16, fontweight='bold')
+axs[0].set_title(nazwyWykresow[0],fontsize=16, fontweight='bold')
+
+                        #rysowanie wykresu pudelkowego
+
+#tworzy dane do wykresu
+data1 = list(myData['species'].drop_duplicates())
+data2 = [myData[myData['species'] == data1[0]]['sepal length'],
+         myData[myData['species'] == data1[1]]['sepal length'],
+         myData[myData['species'] == data1[2]]['sepal length']]
+#oznaczenia na osi x
+
+#wlacza siatke
+axs[1].grid(True, linewidth=0.5)
+#opisy z wlasciwosciami
+axs[1].boxplot(data2, labels=data1,)
+axs[1].set_ylabel('Długość (cm)', fontsize=16, fontweight='bold')
+axs[1].set_xlabel("Gatunek", fontsize=16, fontweight='bold')
+
+
+#zapewnia odpowiednie odstępy między wykresami
+plt.tight_layout()
+plt.show()
+
+
